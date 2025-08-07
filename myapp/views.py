a9774935b1af88543eb6434a2b990d99fc9b0a7c
Saurabh Maurya <saurabhmauryajnp28@gmail.com>
2025-08-07 16:43:21 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -9,10 +9,10 @@ from django.conf import settings
 
 
 
-SCHOOL_NAME = "Prarthana International School"
-Mobile_number="7068323132"
-address='near City Station, Harbaspur, Madarpur, Jaunpur, Uttar Pradesh 222105'
-map_embed_code ='''<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3606.1768997342815!2d82.9868724!3d25.3318443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398e2dddc23c434d%3A0x807c120b49aa211e!2sKIDZEE%20ELEMENTARY%20SCHOOL(%20Alliance%20Partner)KENZEN%20SCHOOL!5e0!3m2!1sen!2sin!4v1752493194518!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'''
+SCHOOL_NAME = "RK sahu junior high school"
+Mobile_number="9999999999"
+address='QP87+G25 ज़मीन, रामपुर, ज़मीन, हिसमपुर, Uttar Pradesh 222001'
+map_embed_code ='''<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3593.130569406629!2d82.71004287523095!3d25.766250477349104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399039010272e529%3A0x98b4e3a8e9f358d6!2sRK%20sahu%20junior%20high%20school!5e0!3m2!1sen!2sin!4v1754565177004!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'''
 
 def home(request):
     notices = Notice.objects.order_by('-created_at')[:5]  # latest 5 notices
